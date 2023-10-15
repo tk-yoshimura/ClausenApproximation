89b2d47772d4d210c2c8a3396f221af271fca39b
tk-yoshimura <yoshimura8192@gmail.com>
2023-10-16 02:15:54 +0900

    add: pade workspace

--- ClausenApproximation/PlotValues.cs
+++ ClausenApproximation/PlotValues.cs
@@ -3,34 +3,34 @@
 namespace ClausenApproximation {
     internal class PlotValues {
         static void Main() {
-            StreamWriter sw = new("../../../../results/clausen2_n32.csv");
+            StreamWriter sw = new("../../../../results/clausen2_n64.csv");
 
             sw.WriteLine($"x,clausen2(x),clausen2(x^2),clausen2(x^2)/(x*(1-x))");
 
-            List<MultiPrecision<Pow2.N32>> xs = new() {
+            List<MultiPrecision<Pow2.N64>> xs = new() {
                 0
             };
 
-            for (MultiPrecision<Pow2.N32> x = MultiPrecision<Pow2.N32>.Ldexp(1, -100); x < 1 / 33554432d; x *= 2) {
+            for (MultiPrecision<Pow2.N64> x = MultiPrecision<Pow2.N64>.Ldexp(1, -100); x < 1 / 33554432d; x *= 2) {
                 xs.Add(x);
             }
-            for (MultiPrecision<Pow2.N32> x = 1 / 33554432d; x < 1 / 16384d; x += 1 / 33554432d) {
+            for (MultiPrecision<Pow2.N64> x = 1 / 33554432d; x < 1 / 16384d; x += 1 / 33554432d) {
                 xs.Add(x);
             }
-            for (MultiPrecision<Pow2.N32> x = 1 / 16384d; x < 16383 / 16384d; x += 1 / 16384d) {
+            for (MultiPrecision<Pow2.N64> x = 1 / 16384d; x < 16383 / 16384d; x += 1 / 16384d) {
                 xs.Add(x);
             }
-            for (MultiPrecision<Pow2.N32> x = 16383 / 16384d; x <= 1; x += 1 / 33554432d) {
+            for (MultiPrecision<Pow2.N64> x = 16383 / 16384d; x <= 1; x += 1 / 33554432d) {
                 xs.Add(x);
             }
 
-            foreach (MultiPrecision<Pow2.N32> x in xs) {
-                MultiPrecision<Pow2.N32> y = Clausen2N32.Value(x);
-                MultiPrecision<Pow2.N32> y2 = Clausen2N32.Value(x * x);
+            foreach (MultiPrecision<Pow2.N64> x in xs) {
+                MultiPrecision<Pow2.N64> y = Clausen2N64.Value(x);
+                MultiPrecision<Pow2.N64> y2 = Clausen2N64.Value(x * x);
 
-                MultiPrecision<Pow2.N32> y_scaled =
+                MultiPrecision<Pow2.N64> y_scaled =
                     x == 0 ? 0 :
-                    x == 1 ? MultiPrecision<Pow2.N32>.PI * MultiPrecision<Pow2.N32>.Log(4) :
+                    x == 1 ? MultiPrecision<Pow2.N64>.PI * MultiPrecision<Pow2.N64>.Log(4) :
                     y2 / (x * (1 - x));
 
                 Console.WriteLine($"{x},{y:e40},{y2:e40},{y_scaled:e40}");
---
+++ ClausenPade/Program.cs
@@ -0,0 +1,87 @@
+﻿using MultiPrecision;
+using MultiPrecisionAlgebra;
+using MultiPrecisionCurveFitting;
+
+namespace ClausenPade {
+    internal class Program {
+        static void Main() {
+
+            List<(MultiPrecision<Pow2.N32> x, MultiPrecision<Pow2.N32> y)> expecteds = new();
+
+            using StreamReader sr = new("../../../../results/clausen2_n32.csv");
+
+            sr.ReadLine();
+            while (!sr.EndOfStream) {
+                string? line = sr.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) {
+                    break;
+                }
+
+                string[] line_split = line.Split(",");
+                MultiPrecision<Pow2.N32> x = line_split[0], y = line_split[3];
+
+                y /= MultiPrecision<Pow2.N32>.PI;
+
+                expecteds.Add((x, y));
+            }
+
+            using StreamWriter sw_result = new("../../../../results_disused/clausen2_e32_pade.csv");
+
+            Vector<Pow2.N32> xs = expecteds.Select(item => item.x).ToArray(), ys = expecteds.Select(item => item.y).ToArray();
+
+            for (int m = 4; m <= 32; m++) {
+                PadeFitter<Pow2.N32> pade = new(xs, ys, m, m, intercept: 0);
+
+                Vector<Pow2.N32> param = pade.ExecuteFitting();
+                Vector<Pow2.N32> errs = pade.Error(param);
+
+                MultiPrecision<Pow2.N32> max_rateerr = 0;
+                for (int i = 0; i < errs.Dim; i++) {
+                    if (ys[i] == 0) {
+                        continue;
+                    }
+
+                    max_rateerr = MultiPrecision<Pow2.N32>.Max(errs[i] / ys[i], max_rateerr);
+                }
+
+                Console.WriteLine($"m={m},n={m}");
+                Console.WriteLine($"{max_rateerr:e20}");
+
+                if (max_rateerr < 2e-32) {
+                    sw_result.WriteLine($"m={m},n={m}");
+                    sw_result.WriteLine("numer");
+                    foreach (var (_, val) in param[..m]) {
+                        sw_result.WriteLine(val);
+                    }
+                    sw_result.WriteLine("denom");
+                    foreach (var (_, val) in param[m..]) {
+                        sw_result.WriteLine(val);
+                    }
+                    sw_result.WriteLine("hexcode");
+                    for (int i = 0; i < m; i++) {
+                        sw_result.WriteLine($"({ToFP128(param[i])}, {ToFP128(param[i + m])}),");
+                    }
+
+                    sw_result.WriteLine("relative err");
+                    sw_result.WriteLine($"{max_rateerr:e20}");
+                    sw_result.Flush();
+
+                    break;
+                }
+            }
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+
+        public static string ToFP128(MultiPrecision<Pow2.N32> x) {
+            Sign sign = x.Sign;
+            long exponent = x.Exponent;
+            uint[] mantissa = x.Mantissa.Reverse().ToArray();
+
+            string code = $"({(sign == Sign.Plus ? "+1" : "-1")}, {exponent}, 0x{mantissa[0]:X8}{mantissa[1]:X8}uL, 0x{mantissa[2]:X8}{mantissa[3]:X8}uL)";
+
+            return code;
+        }
+    }
+}
